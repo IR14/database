8ad62fd93ac309273a6ef4bdbd8dbdca794336a4
IR14 <59391583+IR14@users.noreply.github.com>
2021-03-14 23:42:12 +0300

    Update lab1.sql

--- lab1.sql
+++ lab1.sql
@@ -43,9 +43,10 @@ INSERT INTO Medicine_list VALUES (345678, 'Morphine', 'yes');
 
 INSERT INTO Companies VALUES ('RPharm', 'Russia');
 INSERT INTO Companies VALUES ('LAMC', 'The USA');
+INSERT INTO Companies VALUES ('Никомед', 'Russia');
 
 INSERT INTO Medicines VALUES (11223344, 123456, 'Сapsules', 10, 'RPharm', 1.25, 999, 3.5, 4);
-INSERT INTO Medicines VALUES (22334455, 234567, 'Pills', 20, 'RPharm', 2, 2222, 4.7, 3);
+INSERT INTO Medicines VALUES (22334455, 234567, 'Pills', 20, 'Никомед', 2, 0, 4.7, 3);
 INSERT INTO Medicines VALUES (33445566, 345678, 'Dropper liquid', 1, 'Morphine', 0.07 , 12, 985, 1);
 
 INSERT INTO Only_by_prescription VALUES (33445566, 1234567890, 'Maria Brown', 5552222444, “1999.05.23”, 0.5);
