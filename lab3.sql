db12c5faa60d8ad321f3944ddb9e4a1333270ddd
IR14 <59391583+IR14@users.noreply.github.com>
2021-03-15 17:50:03 +0300

    Update lab3.sql

--- lab3.sql
+++ lab3.sql
@@ -1,1 +1,6 @@
-SELECT a.name, a.release_form, a.dosage, a.minimal_stock, a.pharmacy_amount FROM medicines a
+CREATE or REPLACE VIEW remains AS
+SELECT a.name, a.release_form, a.dosage, a.minimal_stock, a.pharmacy_amount 
+FROM medicines a
+WHERE a.pharmacy_amount < a.minimal_stock;
+
+SELECT * FROM remains;
