86d232b855927da21fefb72bf7bba9bb12b18be6
IR14 <59391583+IR14@users.noreply.github.com>
2021-03-15 23:32:11 +0300

    Update lab3.sql

--- lab3.sql
+++ lab3.sql
@@ -5,19 +5,33 @@ WHERE a.pharmacy_amount < a.minimal_stock;
 
 SELECT * FROM remains;
 
+UPDATE remains
+set release_form = 'Сapsules' WHERE pharmacy_amount = 0;
+DELETE from dosage where dosage > 1000;
+INSERT into pharmacy_amount VALUES (1);
+
 CREATE OR REPLACE view companies_zero AS
 SELECT A.company FROM medicines A
 WHERE A.pharmacy_amount = 0;
 
 SELECT * FROM companies_zero;
 
+UPDATE companies_zero
+SET company = 'RPharm' WHERE company != 'RPharm';
+DELETE FROM company WHERE company = 'RPharm';
+INSERT INTO company VALUES ('NAOS');
+
 CREATE OR REPLACE view recipe AS
-SELECT A.health_insurance,
-	     A.full_name,
+SELECT A.health_insurance, A.full_name,
        (SELECT min(B.date) FROM only_by_prescription B WHERE B.full_name=A.full_name),
        (SELECT MAX(B.date) FROM only_by_prescription B WHERE B.full_name=A.full_name),
-       count(A)
+       count(A) as Purchases
 FROM only_by_prescription A
 GROUP BY A.full_name, A.health_insurance;
 
 SELECT * FROM recipe;
+
+UPDATE recipe
+set purchases = 1 WHERE purchases > 1;
+DELETE from full_name WHERE purchases = 1;
+insert INTO min VALUEs ('2021-03-12');
